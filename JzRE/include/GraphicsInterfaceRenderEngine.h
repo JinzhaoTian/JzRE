58b8c946d5769b908474b902adb59cafb0a0868d
Jinzhao Tian <tianjinzhao@qq.com>
2024-07-11 23:59:42 +0800

    fix gcc build library import error

--- JzRE/include/GraphicsInterfaceRenderEngine.h
+++ JzRE/include/GraphicsInterfaceRenderEngine.h
@@ -7,6 +7,7 @@ namespace JzRE {
 class GraphicsInterfaceRenderEngine {
 public:
     GraphicsInterfaceRenderEngine();
+    ~GraphicsInterfaceRenderEngine();
     void Render();
 
 private:
--- JzRE/source/GraphicsInterfaceRenderEngine.cpp
+++ JzRE/source/GraphicsInterfaceRenderEngine.cpp
@@ -1,12 +1,14 @@
 #include "GraphicsInterfaceRenderEngine.h"
 
 namespace JzRE {
+
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
                                  "void main()\n"
                                  "{\n"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
                                  "}\0";
+
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
                                    "void main()\n"
@@ -16,6 +18,9 @@ const char *fragmentShaderSource = "#version 330 core\n"
 
 GraphicsInterfaceRenderEngine::GraphicsInterfaceRenderEngine() {
     this->window = CreateSharedPtr<GraphicsInterfaceRenderWindow>(this->wndWidth, this->wndHeight, this->title.c_str());
+}
+
+GraphicsInterfaceRenderEngine::~GraphicsInterfaceRenderEngine() {
 }
 
 void GraphicsInterfaceRenderEngine::Render() {
