f155edbf8dc977b3870b05b6594e39e1b62543e2
Jinzhao Tian <jinzhao.tian.cs@gmail.com>
2025-07-30 21:54:45 +0800

    back

---
+++ JzRE/include/JzMatrix.h
@@ -0,0 +1,336 @@
+#pragma once
+
+#include "CommonTypes.h"
+#include "JzVector.h"
+
+namespace JzRE {
+
+template <Size M, Size N, typename T = F32>
+class JzMatrix {
+public:
+    inline JzMatrix()
+    {
+        for (Size i = 0; i < M; ++i) {
+            for (Size j = 0; j < N; ++j) {
+                At(i, j) = T(0);
+            }
+        }
+    }
+
+    inline JzMatrix(const T &value)
+    {
+        for (Size i = 0; i < M; ++i) {
+            for (Size j = 0; j < N; ++j) {
+                At(i, j) = value;
+            }
+        }
+    }
+
+    inline JzMatrix(const T *data)
+    {
+        std::memcpy(m_Data, data, sizeof(m_Data));
+    }
+
+    inline JzMatrix(const JzMatrix &other)
+    {
+        std::memcpy(m_Data, other.m_Data, sizeof(m_Data));
+    }
+
+    inline JzMatrix<M, N, T> &operator=(const JzMatrix<M, N, T> &other)
+    {
+        std::memcpy(m_Data, other.m_Data, sizeof(m_Data));
+        return *this;
+    }
+
+    inline JzMatrix<M, N, T> &operator+=(const JzMatrix<M, N, T> &other)
+    {
+        for (Size i = 0; i < M; ++i) {
+            for (Size j = 0; j < N; ++j) {
+                At(i, j) += other.At(i, j);
+            }
+        }
+        return *this;
+    }
+
+    inline JzMatrix<M, N, T> &operator-=(const JzMatrix<M, N, T> &other)
+    {
+        for (Size i = 0; i < M; ++i) {
+            for (Size j = 0; j < N; ++j) {
+                At(i, j) -= other.At(i, j);
+            }
+        }
+        return *this;
+    }
+
+    inline JzMatrix<M, N, T> &operator*=(const T &value)
+    {
+        for (Size i = 0; i < M; ++i) {
+            for (Size j = 0; j < N; ++j) {
+                m_Data[i][j] *= value;
+            }
+        }
+        return *this;
+    }
+
+    inline JzMatrix<M, N, T> &operator/=(const T &value)
+    {
+        for (Size i = 0; i < M; ++i) {
+            for (Size j = 0; j < N; ++j) {
+                At(i, j) /= value;
+            }
+        }
+        return *this;
+    }
+
+    inline JzMatrix<M, N, T> operator+(const JzMatrix<M, N, T> &other) const
+    {
+        JzMatrix<M, N, T> result  = *this;
+        result                   += other;
+        return result;
+    }
+
+    inline JzMatrix<M, N, T> operator-(const JzMatrix<M, N, T> &other) const
+    {
+        JzMatrix<M, N, T> result  = *this;
+        result                   -= other;
+        return result;
+    }
+
+    inline JzMatrix<M, N, T> operator*(const T &value) const
+    {
+        JzMatrix<M, N, T> result  = *this;
+        result                   *= value;
+        return result;
+    }
+
+    inline JzMatrix<M, N, T> operator/(const T &value) const
+    {
+        JzMatrix<M, N, T> result  = *this;
+        result                   /= value;
+        return result;
+    }
+
+    inline JzMatrix<N, M, T> Transpose() const
+    {
+        JzMatrix<N, M, T> result;
+        for (Size i = 0; i < M; ++i) {
+            for (Size j = 0; j < N; ++j) {
+                result.At(j, i) = At(i, j);
+            }
+        }
+        return result;
+    }
+
+    inline const T &At(Size i, Size j) const
+    {
+        return m_Data[i * N + j];
+    }
+
+    inline T &At(Size i, Size j)
+    {
+        return m_Data[i * N + j];
+    }
+
+    inline const T &operator()(Size i, Size j) const
+    {
+        return At(i, j);
+    }
+
+    inline T &operator()(Size i, Size j)
+    {
+        return At(i, j);
+    }
+
+    inline const T *operator[](Size i) const
+    {
+        return m_Data + i * N;
+    }
+
+    inline T *operator[](Size i)
+    {
+        return m_Data + i * N;
+    }
+
+    // matrix-vector operations
+
+    // 4*4 matrices only
+
+protected:
+    T m_Data[M * N];
+};
+
+using JzMat2 = JzMatrix<2, 2, F32>;
+using JzMat3 = JzMatrix<3, 3, F32>;
+using JzMat4 = JzMatrix<4, 4, F32>;
+
+class JzMat4x4 : public JzMatrix<4, 4, F32> {
+public:
+    inline JzMat4x4()
+    {
+        std::memset(m_Data, 0, sizeof(m_Data));
+        m_Data[0] = m_Data[5] = m_Data[10] = m_Data[15] = 1.0f;
+    }
+
+    inline JzMat4x4(
+        F32 m00, F32 m01, F32 m02, F32 m03,
+        F32 m10, F32 m11, F32 m12, F32 m13,
+        F32 m20, F32 m21, F32 m22, F32 m23,
+        F32 m30, F32 m31, F32 m32, F32 m33)
+    {
+        this->At(0, 0) = m00;
+        this->At(0, 1) = m01;
+        this->At(0, 2) = m02;
+        this->At(0, 3) = m03;
+        this->At(1, 0) = m10;
+        this->At(1, 1) = m11;
+        this->At(1, 2) = m12;
+        this->At(1, 3) = m13;
+        this->At(2, 0) = m20;
+        this->At(2, 1) = m21;
+        this->At(2, 2) = m22;
+        this->At(2, 3) = m23;
+        this->At(3, 0) = m30;
+        this->At(3, 1) = m31;
+        this->At(3, 2) = m32;
+        this->At(3, 3) = m33;
+    }
+
+    inline JzMat4x4(const JzVector<4, F32> &v0, const JzVector<4, F32> &v1, const JzVector<4, F32> &v2, const JzVector<4, F32> &v3)
+    {
+        this->At(0, 0) = v0[0];
+        this->At(0, 1) = v0[1];
+        this->At(0, 2) = v0[2];
+        this->At(0, 3) = v0[3];
+        this->At(1, 0) = v1[0];
+        this->At(1, 1) = v1[1];
+        this->At(1, 2) = v1[2];
+        this->At(1, 3) = v1[3];
+        this->At(2, 0) = v2[0];
+        this->At(2, 1) = v2[1];
+        this->At(2, 2) = v2[2];
+        this->At(2, 3) = v2[3];
+        this->At(3, 0) = v3[0];
+        this->At(3, 1) = v3[1];
+        this->At(3, 2) = v3[2];
+        this->At(3, 3) = v3[3];
+    }
+
+    inline JzMat4x4 Mul(const JzMatrix<4, 4, F32> &other) const
+    {
+        JzMat4x4 result;
+        for (Size i = 0; i < 4; ++i) {
+            for (Size j = 0; j < 4; ++j) {
+                result.At(i, j) = this->At(i, 0) * other.At(0, j) + this->At(i, 1) * other.At(1, j) + this->At(i, 2) * other.At(2, j) + this->At(i, 3) * other.At(3, j);
+            }
+        }
+        return result;
+    }
+
+    inline JzVector<4, F32> Mul(const JzVector<4, F32> &v) const
+    {
+        JzVec4 result;
+        for (Size i = 0; i < 4; ++i) {
+            result[i] = this->At(i, 0) * v[0] + this->At(i, 1) * v[1] + this->At(i, 2) * v[2] + this->At(i, 3) * v[3];
+        }
+        return result;
+    }
+
+    static JzMat4x4 Identity()
+    {
+        return JzMat4x4(
+            1.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    static JzMat4x4 Translate(const JzVector<3, F32> &v)
+    {
+        return JzMat4x4(
+            1.0f, 0.0f, 0.0f, v[0],
+            0.0f, 1.0f, 0.0f, v[1],
+            0.0f, 0.0f, 1.0f, v[2],
+            0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    static JzMat4x4 Scale(const JzVector<3, F32> &v)
+    {
+        return JzMat4x4(
+            v[0], 0.0f, 0.0f, 0.0f,
+            0.0f, v[1], 0.0f, 0.0f,
+            0.0f, 0.0f, v[2], 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    static JzMat4x4 RotateX(F32 angle)
+    {
+        F32 c = std::cos(angle);
+        F32 s = std::sin(angle);
+        return JzMat4x4(
+            1.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, c, -s, 0.0f,
+            0.0f, s, c, 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    static JzMat4x4 RotateY(F32 angle)
+    {
+        F32 c = std::cos(angle);
+        F32 s = std::sin(angle);
+        return JzMat4x4(
+            c, 0.0f, s, 0.0f,
+            0.0f, 1.0f, 0.0f, 0.0f,
+            -s, 0.0f, c, 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    static JzMat4x4 RotateZ(F32 angle)
+    {
+        F32 c = std::cos(angle);
+        F32 s = std::sin(angle);
+        return JzMat4x4(
+            c, -s, 0.0f, 0.0f,
+            s, c, 0.0f, 0.0f,
+            0.0f, 0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    static JzMat4x4 LookAt(const JzVector<3, F32> &eye, const JzVector<3, F32> &center, const JzVector<3, F32> &up)
+    {
+        JzVector<3, F32> z = (eye - center).Normalized();
+        JzVector<3, F32> x = up.Cross(z).Normalized();
+        JzVector<3, F32> y = z.Cross(x).Normalized();
+
+        return JzMat4x4(
+            x[0], x[1], x[2], -x.Dot(eye),
+            y[0], y[1], y[2], -y.Dot(eye),
+            z[0], z[1], z[2], -z.Dot(eye),
+            0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    static JzMat4x4 Perspective(F32 fov, F32 aspect, F32 zNear, F32 zFar)
+    {
+        F32 tanHalfFov = std::tan(fov / 2.0f);
+        F32 zRange     = zNear - zFar;
+        return JzMat4x4(
+            1.0f / (aspect * tanHalfFov), 0.0f, 0.0f, 0.0f,
+            0.0f, 1.0f / tanHalfFov, 0.0f, 0.0f,
+            0.0f, 0.0f, -(-zNear - zFar) / zRange, 2.0f * zFar * zNear / zRange,
+            0.0f, 0.0f, -1.0f, 0.0f);
+    }
+
+    static JzMat4x4 Orthographics(F32 left, F32 right, F32 bottom, F32 top, F32 zNear, F32 zFar)
+    {
+        F32 width  = right - left;
+        F32 height = top - bottom;
+        F32 depth  = zFar - zNear;
+
+        return JzMat4x4(
+            2.0f / width, 0.0f, 0.0f, -(right + left) / width,
+            0.0f, 2.0f / height, 0.0f, -(top + bottom) / height,
+            0.0f, 0.0f, -2.0f / depth, -(zFar + zNear) / depth,
+            0.0f, 0.0f, 0.0f, 1.0f);
+    }
+};
+
+} // namespace JzRE
---
+++ JzRE/include/JzVector.h
@@ -0,0 +1,328 @@
+#pragma once
+
+#include "CommonTypes.h"
+
+namespace JzRE {
+
+template <typename T>
+concept VectorValueType = std::is_arithmetic_v<T>;
+
+template <Size N, typename T = F32>
+class JzVector {
+public:
+    // constructors
+    inline JzVector(const T &value)
+    {
+        for (Size i = 0; i < N; ++i) {
+            m_Data[i] = value;
+        }
+    }
+
+    template <Size M, VectorValueType... Args>
+    inline JzVector(const JzVector<M> &other, Args... args)
+    {
+        if constexpr (sizeof...(args) == 0) {
+            std::memcpy(m_Data, other.m_Data, std::clamp(M, Size(0), N) * sizeof(T));
+        } else if constexpr ((M + sizeof...(args)) >= N) {
+            std::memcpy(m_Data, other.m_Data, std::clamp(M, Size(0), N) * sizeof(T));
+            T data[] = {args...};
+            std::memcpy(m_Data + M, data, sizeof(data));
+        } else {
+            std::memcpy(m_Data, other.m_Data, std::clamp(M, Size(0), N) * sizeof(T));
+            T data[] = {args...};
+            std::memcpy(m_Data + M, data, sizeof(data));
+
+            for (Size i = M + sizeof...(args); i < N; ++i) {
+                m_Data[i] = T();
+            }
+        }
+    }
+
+    template <VectorValueType... Args>
+    inline JzVector(Args... args)
+    {
+        static_assert(sizeof...(args) <= N, "Invalid number of arguments");
+        if constexpr (sizeof...(args) == 0) {
+            for (Size i = 0; i < N; ++i) {
+                m_Data[i] = T();
+            }
+        } else {
+            T data[] = {static_cast<T>(args)...};
+            std::memcpy(m_Data, data, sizeof(data));
+        }
+    }
+
+    inline JzVector(const T *values)
+    {
+        std::memcpy(m_Data, values, N * sizeof(T));
+    }
+
+    template <typename U>
+    inline JzVector(const JzVector<N, U> &source_)
+    {
+        (*this) = source_;
+    }
+
+    inline JzVector<N, T> &operator=(const JzVector<N, T> &other)
+    {
+        std::memcpy(m_Data, other.m_Data, sizeof(m_Data));
+        return *this;
+    }
+
+    inline T &operator[](Size index)
+    {
+        return m_Data[index];
+    }
+
+    inline const T &operator[](Size index) const
+    {
+        return m_Data[index];
+    }
+
+    inline JzVector<N, T> operator+(const JzVector<N, T> &other) const
+    {
+        JzVector<N, T> result;
+        for (Size i = 0; i < N; ++i) {
+            result[i] = m_Data[i] + other[i];
+        }
+        return result;
+    }
+
+    inline JzVector<N, T> operator-(const JzVector<N, T> &other) const
+    {
+        JzVector<N, T> result;
+        for (Size i = 0; i < N; ++i) {
+            result[i] = m_Data[i] - other[i];
+        }
+        return result;
+    }
+
+    inline JzVector<N, T> operator*(const JzVector<N, T> &other) const
+    {
+        JzVector<N, T> result;
+        for (Size i = 0; i < N; ++i) {
+            result[i] = m_Data[i] * other[i];
+        }
+        return result;
+    }
+
+    inline JzVector<N, T> operator/(const JzVector<N, T> &other) const
+    {
+        JzVector<N, T> result;
+        for (Size i = 0; i < N; ++i) {
+            result[i] = m_Data[i] / other[i];
+        }
+        return result;
+    }
+
+    inline JzVector<N, T> operator*(const T &value) const
+    {
+        JzVector<N, T> result;
+        for (Size i = 0; i < N; ++i) {
+            result[i] = m_Data[i] * value;
+        }
+        return result;
+    }
+
+    inline JzVector<N, T> operator/(const T &value) const
+    {
+        JzVector<N, T> result;
+        for (Size i = 0; i < N; ++i) {
+            result[i] = m_Data[i] / value;
+        }
+        return result;
+    }
+
+    inline JzVector<N, T> &operator+=(const JzVector<N, T> &other)
+    {
+        for (Size i = 0; i < N; ++i) {
+            m_Data[i] += other[i];
+        }
+        return *this;
+    }
+
+    inline JzVector<N, T> &operator-=(const JzVector<N, T> &other)
+    {
+        for (Size i = 0; i < N; ++i) {
+            m_Data[i] -= other[i];
+        }
+        return *this;
+    }
+
+    inline JzVector<N, T> &operator*=(const JzVector<N, T> &other)
+    {
+        for (Size i = 0; i < N; ++i) {
+            m_Data[i] *= other[i];
+        }
+        return *this;
+    }
+
+    inline JzVector<N, T> &operator/=(const JzVector<N, T> &other)
+    {
+        for (Size i = 0; i < N; ++i) {
+            m_Data[i] /= other[i];
+        }
+        return *this;
+    }
+
+    inline JzVector<N, T> &operator*=(const T &value)
+    {
+        for (Size i = 0; i < N; ++i) {
+            m_Data[i] *= value;
+        }
+        return *this;
+    }
+
+    inline JzVector<N, T> &operator/=(const T &value)
+    {
+        for (Size i = 0; i < N; ++i) {
+            m_Data[i] /= value;
+        }
+        return *this;
+    }
+
+    inline bool operator==(const JzVector<N, T> &other) const
+    {
+        for (Size i = 0; i < N; ++i) {
+            if ((m_Data[i] - other[i]) > std::numeric_limits<T>::epsilon()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    inline JzVector<N, T> operator-() const
+    {
+        JzVector<N, T> result;
+        for (Size i = 0; i < N; ++i) {
+            result[i] = -m_Data[i];
+        }
+        return result;
+    }
+
+    inline bool operator!=(const JzVector<N, T> &other) const
+    {
+        return !(*this == other);
+    }
+
+    inline T Length() const
+    {
+        T result = 0;
+        for (Size i = 0; i < N; ++i) {
+            result += m_Data[i] * m_Data[i];
+        }
+        return static_cast<T>(std::sqrt(result));
+    }
+
+    inline T LengthSquared() const
+    {
+        T result = 0;
+        for (Size i = 0; i < N; ++i) {
+            result += m_Data[i] * m_Data[i];
+        }
+        return result;
+    }
+
+    inline T Dot(const JzVector<N, T> &other) const
+    {
+        T result = 0;
+        for (Size i = 0; i < N; ++i) {
+            result += m_Data[i] * other[i];
+        }
+        return result;
+    }
+
+    inline JzVector<N, T> Pow(F32 power) const
+    {
+        JzVector<N, T> result;
+        for (Size i = 0; i < N; ++i) {
+            result[i] = std::pow(m_Data[i], power);
+        }
+        return result;
+    }
+
+    inline JzVector<N, T> Cross(const JzVector<N, T> &other) const
+    {
+        static_assert(N == 3 || N == 2, "Cross product is only defined for 3D vectors");
+        if (N == 2) {
+            F32 result = m_Data[0] * other[1] - m_Data[1] * other[0];
+            return JzVector<2, T>(result, 0.0f);
+        } else if (N == 3) {
+            return JzVector<3, T>(
+                m_Data[1] * other[2] - m_Data[2] * other[1],
+                m_Data[2] * other[0] - m_Data[0] * other[2],
+                m_Data[0] * other[1] - m_Data[1] * other[0]);
+        }
+    }
+
+    inline JzVector<N, T> Normalized() const
+    {
+        return *this / Length();
+    }
+
+    inline void Normalize()
+    {
+        *this /= Length();
+    }
+
+    inline const JzVector &negate()
+    {
+        for (Size index = 0; index < N; ++index)
+            m_Data[index] = -m_Data[index];
+        return *this;
+    }
+
+    // element accessors for M <= 4;
+    inline T &x()
+    {
+        return m_Data[0];
+    }
+
+    inline T &y()
+    {
+        return m_Data[1];
+    }
+
+    inline T &z()
+    {
+        return m_Data[2];
+    }
+
+    inline T &w()
+    {
+        return m_Data[3];
+    }
+
+    inline const T &x() const
+    {
+        return m_Data[0];
+    }
+
+    inline const T &y() const
+    {
+        return m_Data[1];
+    }
+
+    inline const T &z() const
+    {
+        return m_Data[2];
+    }
+
+    inline const T &w() const
+    {
+        return m_Data[3];
+    }
+
+    // private:
+    T m_Data[N];
+};
+
+using JzVec2 = JzVector<2, F32>;
+using JzVec3 = JzVector<3, F32>;
+using JzVec4 = JzVector<4, F32>;
+
+using JzIVec2 = JzVector<2, I32>;
+using JzIVec3 = JzVector<3, I32>;
+using JzIVec4 = JzVector<4, I32>;
+
+} // namespace JzRE
