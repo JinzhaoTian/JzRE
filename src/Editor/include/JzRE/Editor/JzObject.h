e7edd7fe4c39fbee8a4a9d6195a2f585f7988dfd
Jinzhao Tian <jinzhao.tian.cs@gmail.com>
2025-10-31 22:16:27 +0800

    feat: add actor-component

---
+++ src/Editor/include/JzRE/Editor/JzObject.h
@@ -0,0 +1,15 @@
+/**
+ * @author    Jinzhao Tian
+ * @copyright Copyright (c) 2025 JzRE
+ */
+
+#pragma once
+
+namespace JzRE {
+/**
+ * @brief Base class for all objects in JzRE
+ */
+class JzObject {
+};
+
+} // namespace JzRE
