55d9b6ff8d293d5e1073c45d746d09db7e6f701b
Jinzhao Tian <jinzhao.tian.cs@gmail.com>
2026-01-11 17:43:52 +0800

    refactor: clean up old code

--- src/Core/src/JzServiceContainer.cpp
+++
@@ -1,8 +0,0 @@
-/**
- * @author    Jinzhao Tian
- * @copyright Copyright (c) 2025 JzRE
- */
-
-#include "JzRE/Core/JzServiceContainer.h"
-
-std::unordered_map<JzRE::I32, std::any> JzRE::JzServiceContainer::__SERVICES;
--- src/UI/src/JzActorContextMenu.cpp
+++
@@ -1,9 +0,0 @@
-#include "JzRE/UI/JzActorContextMenu.h"
-
-JzRE::JzActorContextMenu::JzActorContextMenu() { }
-
-void JzRE::JzActorContextMenu::Execute(JzEPluginExecutionContext context)
-{
-    if (m_widgets.size() > 0)
-        JzContextMenu::Execute(context);
-}
